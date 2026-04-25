76d7edd4acf195ee58204b24d92b7efb7e290db0
Al-Amin Abdulkadir <alaminabdulkadir5@gmail.com>
2026-04-25 22:43:27 +0100

    Delete SARA_CRM directory

--- SARA_CRM/project_sara/core/db.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from app.config.settings import settings
-
-engine = create_engine(settings.database_url)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
--- SARA_CRM/project_sara/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from models.customer import Customer, CustomerStatus
-from models.company import Company, CompanySize, CompanyStatus
-from models.interaction import Interaction, InteractionType
-from models.deal import Deal, DealStage
-from models.notification import Notification, NotificationType
-
